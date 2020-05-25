2e47cb8e2e19b03ddc74e6d8f300c2260e856b54
AdriTorrado14 <46044448+AdriTorrado14@users.noreply.github.com>
2020-05-25 19:31:21 +0200

    Update linea.py

--- acquisition/ficheros_modificados/linea.py
+++ acquisition/ficheros_modificados/linea.py
@@ -3,38 +3,38 @@ import math
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
-class Linea(QtWidgets.QGraphicsItem):
+class Linea (QtWidgets.QGraphicsItem):
     def __init__(self, a, b):
         super(Linea, self).__init__()
         self.a = a
         self.b = b
-        
-        self.xPos = (self.a.xPos + self.b.xPos) / 2
-        self.yPos = (self.a.yPos + self.b.yPos) / 2
 
-        s1 = self.a.xPos - self.b.xPos
-        s2 = self.a.yPos - self.b.yPos
+        XPuntoMedio = (self.a.xPos + self.b.xPos) / 2
+        YPuntoMedio = (self.a.yPos + self.b.yPos) / 2
+
+        co1x = self.a.xPos
+        co1y = self.a.yPos
+        co1_2x = self.b.xPos
+        co1_2y = self.b.yPos
+
+        s1 = co1x - co1_2x
+        s2 = co1y - co1_2y
         d = math.sqrt(s1*s1 + s2*s2)
-        angle = math.atan2(s2, s1)
-        
+        self.length = d
 
-        self.length = d # Distancia entre ellos
-        self.setPos(self.xPos, self.yPos) # Punto medio
-        self.BoundingRect = QtCore.QRectF(-d/2, 0, d, 0)
-        self.setRotation(angle*180./math.pi)
-        self.text = QtGui.QStaticText("dis")
+        self.text = QtGui.QStaticText(str(self.length/100))
 
-        #self.punto1 = QtCore.QPointF(self.hum.xPos, self.hum.yPos) # Asignación punto1.
-        #self.punto2 = QtCore.QPointF(self.hum2.xPos, self.hum2.yPos) # Asignación punto2.
-        self.puntomedio = QtCore.QPointF(self.xPos, self.yPos) # Asignación punto medio.
-        #self.lineaNueva = QtCore.QLineF(self.punto1, self.punto2) #Linea entre humano.
+        self.puntoMedio = QtCore.QPointF(XPuntoMedio, YPuntoMedio)
+        self.BoundingRect = QtCore.QRectF(-10, -10, 20, 20) 
+
+        punto1 = QtCore.QPointF(co1x, co1y) # Asignación punto1.
+        punto2 = QtCore.QPointF(co1_2x, co1_2y) # Asignacion punto2
+        self.lineaNueva = QtCore.QLineF(punto1, punto2) #Asignacion nueva linea.
         
     def boundingRect(self):
         return self.BoundingRect
 
-
     def paint(self, painter, option, widget):
-            if (self.length > 40):
-                painter.drawRect(-self.length/2, 0, self.length, 0)
-                painter.drawStaticText(self.puntomedio, self.text)
-
+        if (self.length > 40):
+            painter.drawLine(self.lineaNueva)
+            painter.drawStaticText(self.puntoMedio, self.text)
