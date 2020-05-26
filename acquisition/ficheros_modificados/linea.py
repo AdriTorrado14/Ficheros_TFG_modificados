7056e018e9b9367af75c38e7b2b9c71721e967dd
AdriTorrado14 <46044448+AdriTorrado14@users.noreply.github.com>
2020-05-26 19:27:44 +0200

    Update linea.py

--- acquisition/ficheros_modificados/linea.py
+++ acquisition/ficheros_modificados/linea.py
@@ -19,22 +19,25 @@ class Linea (QtWidgets.QGraphicsItem):
 
         s1 = co1x - co1_2x
         s2 = co1y - co1_2y
-        d = math.sqrt(s1*s1 + s2*s2)
-        self.length = d
+        d = math.sqrt(s1*s1 + s2*s2) # Distancia existente entre las dos coordenadas
+        dis = round(d, 2) # Redondeo de la distancia a 2 cifras decimales.
 
-        self.text = QtGui.QStaticText(str(self.length/100))
+        self.length = dis # Asignación de la distantcia.
 
-        self.puntoMedio = QtCore.QPointF(XPuntoMedio, YPuntoMedio)
+        self.text = QtGui.QStaticText(str(self.length/100) + ' m') # Texto
+
+        self.puntoMedio = QtCore.QPointF(XPuntoMedio, YPuntoMedio) # Asignación punto medio.
         self.BoundingRect = QtCore.QRectF(-10, -10, 20, 20) 
 
         punto1 = QtCore.QPointF(co1x, co1y) # Asignación punto1.
         punto2 = QtCore.QPointF(co1_2x, co1_2y) # Asignacion punto2
-        self.lineaNueva = QtCore.QLineF(punto1, punto2) #Asignacion nueva linea.
+        self.lineaNueva = QtCore.QLineF(punto1, punto2) # Asignacion nueva linea.
         
     def boundingRect(self):
         return self.BoundingRect
 
     def paint(self, painter, option, widget):
-        if (self.length > 40):
+        if (self.length > 35):
             painter.drawLine(self.lineaNueva)
             painter.drawStaticText(self.puntoMedio, self.text)
+
